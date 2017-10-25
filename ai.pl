be560462502c7b4bec025f76cc19275cdc7be84e
togaerror <togaerror@gmail.com>
2017-10-25 16:11:26 +0900

    board関数の作成　未実行

--- ai.pl
+++ ai.pl
@@ -2,9 +2,37 @@ use strict;
 use warnings;
 use utf8;
 use IO::Socket;
-
+use Data::Dumper;
 &main();
 exit;
+
+sub violation {
+	my ($msg, $log) = @_;
+	
+
+}
+sub board {
+	my ($msg) = @_;
+	my @keep = split /\,/, $msg;
+	print "@keep\n";
+	
+	my @board;
+	for(my $i = 0; $i <= $#keep; $i += 2) {
+		if($keep[$i] =~ /A./) {
+			push(@{$board[0]}, $keep[$i + 1]);
+		} elsif($keep[$i] =~ /B./) {
+			push(@{$board[1]}, $keep[$i + 1]);
+		} elsif($keep[$i] =~ /C./) {
+			push(@{$board[2]}, $keep[$i + 1]);
+		} elsif($keep[$i] =~ /D./) {
+			push(@{$board[3]}, $keep[$i + 1]);
+		} elsif($keep[$i] =~ /E./) {
+			push(@{$board[4]}, $keep[$i + 1]);
+		}
+	}
+	print Dumper @board;
+	return (@board);
+}
 
 sub main {
 	print "IP:";
@@ -16,17 +44,31 @@ sub main {
 		PeerPort => $port,
 		Proto => 'tcp');
  		die "IO::Socket : $!" unless $sock;	
-	
 	my $msg = <$sock>;
+	print $msg;
+	chomp $msg;
+	$msg =~ s/\D//g;
+	my $player = "Player" . $msg;
+	print "$player\n";
+
+	print $sock "initboard\n";
+	$msg = <$sock>;
 	print "$msg\n";
-		
-	while(1) {
-		my $input = <>;
-		last if($input eq "exit");
-		print $sock $input;
-		my $msg = <$sock>;
-		print $msg;
+	my @log;
+	push(@log, "start");
+	
+	#初手
+	my $mv;
+	my @board;
+	if(($log[-1] eq "start")and($player eq "Player1")) {
+		(@board) = &make_board($msg);
+		$mv = &mv_make($msg);
 	}
+	while(1) {
+		my $mv;
+		print $sock "board\n";
+		$msg = <$sock>; chomp $msg;	
+	}	
 	
 	close($sock);
 	print("bye\n");
